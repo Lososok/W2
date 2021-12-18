765163ca0b5897a9290a4d2f459c21718bc6a6bb
Lososok <Froggod217@gmail.com>
2021-12-18 20:14:10 +0300

    implemented a system for evaluating the hit of a click in the ball

--- itsgame.py
+++ itsgame.py
@@ -1,10 +1,45 @@
 import pygame
 from pygame.draw import *
+from random import randint
 
 pygame.init()
 
-FPS = 30
-screen = pygame.display.set_mode((400, 400))
+FPS = 2
+screen = pygame.display.set_mode((1200, 800))
+    
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+def new_ball():
+    '''
+    draw new ball
+    '''
+    global x, y, r
+    x = randint(100, 1100)
+    y = randint(100, 700)
+    r = randint(10, 100)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+def click(event):
+    '''
+    fun prints coordinates of point of the center ball and point of click
+    and says it's inside or outside
+    event - show coordinates of points
+    '''
+    print(x, y, r)
+    print(event.pos)
+    if (x - event.pos[0])**2 + (y - event.pos[1])**2 <= r**2:
+        print('Yes')
+    else:
+        print('No')
+        
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -15,5 +50,10 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click(event)
+    new_ball()
+    pygame.display.update()
+    screen.fill(BLACK)
 
 pygame.quit()
